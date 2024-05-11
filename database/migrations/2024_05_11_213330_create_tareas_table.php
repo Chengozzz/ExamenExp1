9b790a36034d56e3ad92cf5155a6ba5c3c4eb131
Ricardo Gothelf <147527264+Oyasumi-owo@users.noreply.github.com>
2024-05-11 15:48:35 -0600

    Created migrations

---
+++ database/migrations/2024_05_11_213330_create_tareas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tareas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('titulo');
+            $table->text('descripcion')->nullable();
+            $table->date('fecha_entrega');
+            $table->boolean('completado')->default(false);
+            $table->string('categoria')->nullable();
+            $table->sofDeletes();
+           
+            $table->timestamps();
+    
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tareas');
+    }
+};
